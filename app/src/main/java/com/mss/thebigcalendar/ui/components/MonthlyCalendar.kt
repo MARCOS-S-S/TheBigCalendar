dc1cba1133d54a7426eed956ee451c749468ccd1
Marcos Silqueira Sabino <marcossilqueira565@gmail.com>
2025-08-26 08:28:03 -0300

    Improving the monthly calendar

--- app/src/main/java/com/mss/thebigcalendar/ui/components/MonthlyCalendar.kt
+++ app/src/main/java/com/mss/thebigcalendar/ui/components/MonthlyCalendar.kt
@@ -120,8 +120,8 @@ private fun DayCell(
         Text(
             text = day.date.dayOfMonth.toString(),
             textAlign = TextAlign.Center,
-            style = if (day.isSelected) MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold)
-            else MaterialTheme.typography.bodySmall,
+            style = if (day.isSelected) MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold)
+            else MaterialTheme.typography.bodyMedium,
             color = when {
                 day.isSelected -> MaterialTheme.colorScheme.onPrimaryContainer
                 day.isNationalHoliday -> MaterialTheme.colorScheme.error
