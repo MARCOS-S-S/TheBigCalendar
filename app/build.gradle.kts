e59197591359dd9bb4271f58ecd4324230204ac1
Marcos Silqueira Sabino <marcossilqueira565@gmail.com>
2025-09-25 15:56:50 -0300

    Version 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.mss.thebigcalendar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
